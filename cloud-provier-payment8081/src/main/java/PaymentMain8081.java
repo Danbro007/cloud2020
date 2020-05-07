b3e6c56fa81347f188734b30c16edb2b89de4ffd
danbro <710170342@qq.com>
2020-05-07 14:27:57 +0800

    搭建Cloud-Payment-Service服务所需的环境

---
+++ cloud-provier-payment8081/src/main/java/PaymentMain8081.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname PaymentMain8081
+ * @Description TODO
+ * @Date 2020/5/7 14:02
+ * @Author Danrbo
+ */
+@SpringBootApplication
+public class PaymentMain8081 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8081.class);
+    }
+}
