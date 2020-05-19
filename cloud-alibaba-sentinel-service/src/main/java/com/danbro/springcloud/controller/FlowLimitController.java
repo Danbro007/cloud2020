cb5355131267c85be0ccb5c0296c7ed713727ba3
danbro <710170342@qq.com>
2020-05-19 15:30:47 +0800

    添加热点参数限流的测试代码

--- cloud-alibaba-sentinel-service/src/main/java/com/danbro/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service/src/main/java/com/danbro/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.danbro.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -42,10 +45,26 @@ public class FlowLimitController {
         int i = 2 / 0;
         return "-----test 异常比例-----";
     }
+
     @GetMapping("/testE")
     public String testE() {
         int i = 2 / 0;
         return "-----test 异常数-----";
     }
 
+
+    @SentinelResource(value = "testKey", blockHandler = "handleTestHotKey")
+    @GetMapping("/testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        int i = 2 / 0;
+        return "test HotKey Success!";
+    }
+
+    /**
+     * testHotKey 的兜底降级方法
+     */
+    public String handleTestHotKey(String p1, String p2, BlockException ex) {
+        return "handle testHotKey";
+    }
 }
---
+++ cloud-alibaba-sentinel-service/src/main/java/com/danbro/springcloud/controller/RateLimitController.java
@@ -0,0 +1,14 @@
+package com.danbro.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname RateLimitController
+ * @Description TODO
+ * @Date 2020/5/19 14:16
+ * @Author Danrbo
+ */
+@RestController
+public class RateLimitController {
+
+}
